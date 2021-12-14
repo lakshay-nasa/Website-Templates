ede533d70b52f87b4dc14d6813138ecc26fee78e
lakshay-nasa <lakshay1717@gmail.com>
2021-12-14 22:01:34 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def Home():
+    return render_template("index.html")
+
+@app.route("/index")
+def index_page():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
